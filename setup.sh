6c3948faf9c4aa5463f5aeca5050c346b659cc8a
ShengQi <shengqi2018@pku.edu.cn>
2021-11-05 09:37:16 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -exu
 
+workspace=$PWD
+cd ..
+wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-2.2.4.0/MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64.tgz
+tar xvf MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64
+sudo ./mlnxofedinstall
+sudo /etc/init.d/openibd restart
+cd $workspace
+
+sudo apt-get install -y libnuma-dev clang
+
 if [ -f $HOME/.cargo/env ]; then
     source $HOME/.cargo/env
 fi
