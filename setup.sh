0f8b1b4115188a1c974da87e727077f33ea8f409
xinjincs <xinjincs@c4.dev.kayakext-pg0.utah.cloudlab.us>
2021-11-05 20:36:47 -0600

    echo yes to MLNX driver installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cd ..
 wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-2.2.4.0/MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64.tgz
 tar xvf MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64.tgz
 cd MLNX_OFED_LINUX-4.9-2.2.4.0-ubuntu18.04-x86_64
-sudo ./mlnxofedinstall
+echo "y" | sudo ./mlnxofedinstall 
 sudo /etc/init.d/openibd restart
 cd $workspace
 
