69713fb901ac29e95caaf8e6d3a660e9e2ff65d5
xinjincs <xinjincs@server.dev.kayakext-pg0.utah.cloudlab.us>
2022-01-05 02:11:25 -0700

    impl scalar and auth

--- ext/vector/src/lib.rs
+++ ext/vector/src/lib.rs
@@ -23,8 +23,8 @@ extern crate db;
 #[macro_use]
 extern crate sandstorm;
 
-#[macro_use]
-extern crate cfg_if;
+// #[macro_use]
+// extern crate cfg_if;
 
 use std::ops::Generator;
 use std::pin::Pin;
@@ -34,10 +34,22 @@ use sandstorm::db::DB;
 use sandstorm::pack::pack;
 use std::convert::TryInto;
 
-const K: usize = 5; // for topk
+extern crate openssl;
+use openssl::aes::{aes_ige, AesKey};
+use openssl::symm::Mode;
+extern crate crypto;
+use crypto::scrypt::{scrypt, ScryptParams};
+
+// for topk
+const K: usize = 5;
+// for auth
+const SCRYPT_PARAMS: (u8, u32, u32) = (3, 2, 2);
+const AES_KEY: &[u8] = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+static mut AES_IV: [u8; 32] = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
+        \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 
 enum QueryOp {
-    Vector = 1, // TODO: impl multiget
+    Vector = 1,
     TopK = 2,
     Scalar = 3,
     Auth = 4,
@@ -56,18 +68,32 @@ impl From<u8> for QueryOp {
     }
 }
 
+#[no_mangle]
+#[allow(unreachable_code)]
+#[allow(unused_assignments)]
+pub fn init(db: Rc<DB>) -> Pin<Box<Generator<Yield = u64, Return = u64>>> {
+    Box::pin(move || {
+        return dispatch(db);
+        yield 0;
+    })
+}
+
 fn dispatch(db: Rc<DB>) -> u64 {
     let args = db.args();
     let (table, args) = args.split_at(8);
     let (value_len, args) = args.split_at(8);
-    // let (value_len, args) = args.split_at(8);
     let (record_len, args) = args.split_at(4);
-    let (opcode, key) = args.split_at(1);
+    let (opcode, args) = args.split_at(1);
     let table = u64::from_le_bytes(table.try_into().unwrap());
-    let record_len = u32::from_le_bytes(record_len.try_into().unwrap());
+    let value_len = usize::from_le_bytes(record_len.try_into().unwrap());
+    let record_len = u32::from_le_bytes(record_len.try_into().unwrap()) as usize;
     let opcode: QueryOp = opcode[0].into();
-    let key = key.to_vec();
-    return 0;
+    match opcode {
+        QueryOp::Vector => vector_query_handler(db, table, value_len, record_len, args),
+        QueryOp::TopK => topk_query_handler(db, table, value_len, record_len, args),
+        QueryOp::Scalar => scalar_query_handler(db, table, value_len, record_len, args),
+        QueryOp::Auth => auth_query_handler(db, table, value_len, record_len, args),
+    }
 }
 
 fn reinterpret<T>(src: &[u8]) -> &[T] {
@@ -98,27 +124,18 @@ fn vector_query_handler(
     table: u64,
     value_len: usize,
     record_len: usize,
-    key: Vec<u8>,
+    args: &[u8],
 ) -> u64 {
+    let key = args.to_vec();
     let mut obj = None;
     GET!(db, table, key, value_len, obj);
     if let Some(val) = obj {
-        // let mut res = vec![0usize; record_len];
-        // let mut num_records = 0usize;
-        // for v in val.read().chunks(record_len) {
-        //     num_records += 1;
-        //     for (x, &y) in res.iter_mut().zip(v.iter()) {
-        //         *x += y as usize;
-        //     }
-        // }
-        // let res = res.iter().map(|&x| (x / num_records) as u8).collect();
-        // db.resp(&res[..]);
         let mut mean = vec![0f32; record_len / 4];
-        let mut records = 0f32;
+        let mut num_records = 0f32;
         for v in val.read().chunks(record_len) {
             num_records += 1.0;
             let v = reinterpret(v);
-            add(mean, v);
+            add(&mut mean, v);
         }
         for x in mean.iter_mut() {
             *x /= num_records;
@@ -133,16 +150,19 @@ fn vector_query_handler(
     db.resp(error.as_bytes());
     return 1;
 }
-// TODO: transmute this into vec of f32
+
 fn topk(src_vec: Vec<&[f32]>, assoc_vecs: Vec<Vec<&[f32]>>) -> [usize; K] {
-    // TODO: sort
-    let mut order: Vec<usize> = (0..assoc_vecs.len()).collect();
-    order.sort_by_cached_key(|&idx| {
-        let vecs: Vec<_> = ass
-            .chunks(record_len)
-            .map(|record| u8_to_f32(record))
-            .collect();
-    });
+    let mut scores = vec![];
+    for assoc_vec in &assoc_vecs {
+        let mut score = 1f32;
+        for (x, y) in assoc_vec.iter().zip(src_vec.iter()) {
+            score *= dot(x, y);
+        }
+        scores.push(score);
+    }
+    let mut order: Vec<usize> = (0..scores.len()).collect();
+    // in descending order
+    order.sort_by(|&idx1, &idx2| scores[idx2].partial_cmp(&scores[idx1]).unwrap());
     order[..K].try_into().unwrap()
 }
 
@@ -151,8 +171,9 @@ fn topk_query_handler(
     table: u64,
     value_len: usize,
     record_len: usize,
-    mut key: Vec<u8>,
+    args: &[u8],
 ) -> u64 {
+    let mut key = args.to_vec();
     let key_len = key.len() as u16;
     // expected table order: id_assoc = id_vector + 1
     let assoc_table = table + 1;
@@ -174,132 +195,90 @@ fn topk_query_handler(
             .collect::<Vec<_>>();
         let topk = topk(src_vec, assoc_vecs);
         for &idx in &topk {
-            let offset = idx * key_len as usize;
-            db.resp(&assoc_keys[offset..offset + key_len as usize]);
-            return 0;
+            // let offset = idx * key_len as usize;
+            // db.resp(&assoc_keys[offset..offset + key_len as usize]);
+            db.resp(assoc_keys[idx]);
         }
+        return 0;
     }
     let error = "Object does not exist";
     db.resp(error.as_bytes());
     return 1;
 }
 
-#[no_mangle]
-#[allow(unreachable_code)]
-#[allow(unused_assignments)]
-pub fn init(db: Rc<DB>) -> Pin<Box<Generator<Yield = u64, Return = u64>>> {
-    Box::pin(move || {
-        let mut obj = None;
-        let mut t_table: u64 = 0;
-        let mut num: u32 = 0;
-        let mut ord: u32 = 0;
-        let mut keys: Vec<u8> = Vec::with_capacity(30);
-
-        {
-            // First off, retrieve the arguments to the extension.
-            let args = db.args();
-
-            // Check that the arguments received is long enough to contain an
-            // 8 byte table id and a key to be looked up. If not, then write
-            // an error message to the response and return to the database.
-            if args.len() <= 8 {
-                let error = "Invalid args";
-                db.resp(error.as_bytes());
-                return 1;
-            }
-
-            // Next, split the arguments into a view over the table identifier
-            // (first eight bytes), and a view over the key to be looked up.
-            // De-serialize the table identifier into a u64.
-            let (table, value) = args.split_at(8);
-            let (number, value) = value.split_at(4);
-            let (order, key) = value.split_at(4);
-            keys.extend_from_slice(key);
-
-            for (idx, e) in table.iter().enumerate() {
-                t_table |= (*e as u64) << (idx << 3);
-            }
-
-            for (idx, e) in number.iter().enumerate() {
-                num |= (*e as u32) << (idx << 3);
-            }
-
-            for (idx, e) in order.iter().enumerate() {
-                ord |= (*e as u32) << (idx << 3);
-            }
+fn scalar_query_handler(
+    db: Rc<DB>,
+    table: u64,
+    value_len: usize,
+    record_len: usize,
+    args: &[u8],
+) -> u64 {
+    let key = args.to_vec();
+    let mut obj = None;
+    GET!(db, table, key, value_len, obj);
+    if let Some(val) = obj {
+        let mut mean = vec![0f32; record_len / 4];
+        let mut num_records = 0f32;
+        for v in val.read().chunks(record_len) {
+            num_records += 1.0;
+            let v = reinterpret(v);
+            add(&mut mean, v);
         }
-
-        let mut mul: u64 = 0;
-
-        for i in 0..num {
-            // println!("round {} key {:?}", i, keys);
-            GET!(db, t_table, keys, obj);
-
-            if i == num - 1 {
-                match obj {
-                    // If the object was found, use the response.
-                    Some(val) => {
-                        mul = val.read()[0] as u64;
-                    }
-
-                    // If the object was not found, write an error message to the
-                    // response.
-                    None => {
-                        let error = "Object does not exist";
-                        db.resp(error.as_bytes());
-                        return 1;
-                    }
-                }
-            } else {
-                // find the key for the second request.
-                match obj {
-                    // If the object was found, find the key from the response.
-                    Some(val) => {
-                        keys[0..4].copy_from_slice(&val.read()[0..4]);
-                    }
-
-                    // If the object was not found, write an error message to the
-                    // response.
-                    None => {
-                        let error = "Object does not exist";
-                        db.resp(error.as_bytes());
-                        return 1;
-                    }
-                }
-            }
+        for x in mean.iter_mut() {
+            *x /= num_records;
         }
-
-        // if ord >= 600 {
-        //     let start = cycles::rdtsc();
-        //     while cycles::rdtsc() - start < 600 as u64 {}
-        //     ord -= 600;
-        //     yield 0;
-        // }
-
-        // Compute part for this extension
-        cfg_if! {
-            if #[cfg(feature = "yield")]{
-                loop {
-                    if ord <= 2000 {
-                        let start = cycles::rdtsc();
-                        while cycles::rdtsc() - start < ord as u64 {}
-                        break;
-                    } else {
-                        let start = cycles::rdtsc();
-                        while cycles::rdtsc() - start < 2000 as u64 {}
-                        ord -= 2000;
-                        yield 0;
-                    }
-                }
-            }else{
-                let start = cycles::rdtsc();
-                while cycles::rdtsc() - start < ord as u64 {}
-            }
-        }
-
-        db.resp(pack(&mul));
+        db.resp(std::slice::from_raw_parts(
+            mean.as_ptr() as *const u8,
+            mean.len() * 4,
+        ));
         return 0;
+    }
+    let error = "Object does not exist";
+    db.resp(error.as_bytes());
+    return 1;
+}
 
-        yield 0;
-    })
+fn auth_query_handler(
+    db: Rc<DB>,
+    table: u64,
+    value_len: usize,
+    key_len: usize,
+    args: &[u8],
+) -> u64 {
+    // decrypt aes-encrypted passwd
+    let (key, passwd) = args.split_at(key_len);
+    let key = key.to_vec();
+    let mut passwd = passwd.to_vec();
+    let mut decrpyted = [0u8; 16];
+    let ekey = AesKey::new_decrypt(AES_KEY).unwrap();
+    aes_ige(
+        &passwd[0..16],
+        &mut decrpyted,
+        &ekey,
+        &mut AES_IV,
+        Mode::Decrypt,
+    );
+    passwd[0..16].copy_from_slice(&decrpyted[0..16]);
+    // hash passwd and compare
+    let mut obj = None;
+    GET!(db, table, key, value_len, obj);
+    if let Some(val) = obj {
+        let (hash, salt) = val.read().split_at(24);
+        let mut scrypt_hash = [0u8; 24];
+        scrypt(
+            &passwd,
+            salt,
+            &ScryptParams::new(SCRYPT_PARAMS.0, SCRYPT_PARAMS.1, SCRYPT_PARAMS.2),
+            &mut scrypt_hash,
+        );
+        if scrypt_hash == hash {
+            db.resp("Ok".as_bytes());
+        } else {
+            db.resp("Failed".as_bytes());
+        }
+        return 0;
+    }
+    let error = "Object does not exist";
+    db.resp(error.as_bytes());
+    return 1;
 }
